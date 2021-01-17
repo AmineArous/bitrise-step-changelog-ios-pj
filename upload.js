a56b339f7a3768673c1087374a16a24fe62557fc
Amine Arous <aminearous@live.fr>
2021-01-17 14:21:32 +0100

    Add Changelog

--- upload.js
+++ upload.js
@@ -13,5 +13,16 @@ console.log(
 for (var i = 0; i < arrayJiras.length; i++) {
     var s2 = arrayJiras[i].substring(1).replace(/ /g,'');
     console.log(s2);
+	
 
+	
+	const request = new XMLHttpRequest();
+	request.open('GET', 'https://jira.solocal.com/rest/api/2/issue/${s2}fields=status', false, 'bbm','5Ef-pS7-bPZ-wJ6')
+	request.onreadystatechange = function() {
+	        // D some business logics here if you receive return
+	   if(request.readyState === 4 && request.status === 200) {
+	       console.log(request.responseText);
+	   }
+	}
+	request.send()
 }
