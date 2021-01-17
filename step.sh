93fcf9955c2e71bb0579ae2a46949af5ff599de9
Amine Arous <aminearous@live.fr>
2021-01-17 12:45:20 +0100

    Add Changelog

--- step.sh
+++ step.sh
@@ -7,13 +7,31 @@ echo ${BITRISE_GIT_BRANCH}
 echo "Hello World!"
 echo ${BITRISEIO_GIT_BRANCH_DEST}
 
+git checkout origin/${BITRISEIO_GIT_BRANCH_DEST}
+lastLMasterTag=$(git log --pretty=format:'%h' -n 1)
+#echo $(lastLMasterTag)
+git checkout origin/${BITRISE_GIT_BRANCH}
+lastLBranchTag=$(git log --pretty=format:'%h' -n 1)
+#echo $(lastLBranchTag)
+
+#previousTag=$(git merge-base origin/${BITRISE_GIT_BRANCH} ${BITRISEIO_GIT_BRANCH_DEST})
+#argument=$(git log --no-merges ${BITRISEIO_GIT_BRANCH_DEST}..)
+#argument=$(git log ${BITRISE_GIT_BRANCH} --not $previousTag)
+#argument=$(git log origin/master..origin/test)
+argument2=$(git cherry -v origin/master origin/test)
+
+changelog="$(git log --pretty=format:"%s" $lastLMasterTag...$lastLBranchTag)"
+
+#echo $(changelog)
+echo "--"
+echo $changelog
 
 #
 # --- Export Environment Variables for other Steps:
 # You can export Environment Variables for other Steps with
 #  envman, which is automatically installed by `bitrise setup`.
 # A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
+envman add --key EXAMPLE_STEP_OUTPUT --value $changelog
 # Envman can handle piped inputs, which is useful if the text you want to
 # share is complex and you don't want to deal with proper bash escaping:
 #  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
