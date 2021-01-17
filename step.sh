b57b0ee5fddb22bbc2355218e50c58184e3f20d4
AmineArous <aminearous@live.fr>
2021-01-17 15:03:09 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 set -ex
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-npm install --save
+npm install xhr2 --save
 
 echo "This is the value specified for the input 'example_step_input': ${example_step_input} A"
 
