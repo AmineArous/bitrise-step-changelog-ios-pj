21101de1847eac1359f0124d4f9fc3dba31dfab5
Amine Arous <aminearous@live.fr>
2021-01-17 13:58:46 +0100

    Add Changelog

--- upload.js
+++ upload.js
@@ -4,6 +4,14 @@ let commits = process.argv[2];
 
 console.log('commits', commits);
 
-let arrayJiras = commits.match(/(#.*)/g)
 
-console.log('arrayJiras', arrayJiras);
+let arrayJiras = commits.match(/(#.*) /g);
+console.log(
+  arrayJiras
+);
+
+for (var i = 0; i < arrayJiras.length; i++) {
+    var s2 = arrayJiras[i].substring(1).replace(/ /g,'');
+    console.log(s2);
+
+}
