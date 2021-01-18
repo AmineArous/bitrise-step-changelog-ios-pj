17a660795b810531d14796676d8e9ec7a6d57cd6
AmineArous <aminearous@live.fr>
2021-01-18 22:52:23 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -33,12 +33,12 @@ changelog="$(git log --pretty=format:"%s" $lastLMasterTag...$lastLBranchTag)"
 echo "--"
 echo $changelog
 echo "-------"
-changelog=$($THIS_SCRIPT_DIR/upload.js "${changelog}")
+changelog_final=$($THIS_SCRIPT_DIR/upload.js "${changelog}")
 
 
 
 echo "--final changelog"
-echo $changelog
+echo ${changelog_final}
 echo "-------"
 
 #
@@ -46,7 +46,7 @@ echo "-------"
 # You can export Environment Variables for other Steps with
 #  envman, which is automatically installed by `bitrise setup`.
 # A very simple example:
-envman add --key AMINE_CHANGELOG --value $changelog
+envman add --key AMINE_CHANGELOG --value $changelog_final
 # Envman can handle piped inputs, which is useful if the text you want to
 # share is complex and you don't want to deal with proper bash escaping:
 #  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
